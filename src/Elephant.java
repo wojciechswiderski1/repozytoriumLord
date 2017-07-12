119e19756eb04d56486f9d72438db2fdc8c7a0a7
wojciechswiderski1 <wojciechswiderski87@gmail.com>
2017-07-12 20:30:59 +0200

    dodanie klasy Elephant

---
+++ src/Elephant.java
@@ -0,0 +1,18 @@
+/**
+ * Created by RENT on 2017-07-12.
+ */
+public class Elephant {
+    private int a;
+
+    public Elephant(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
